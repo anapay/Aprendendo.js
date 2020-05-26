6006e995911ecf5f98c3a1b528816f69c2cfc7b7
anapay <anapay42@gmail.com>
2020-05-26 18:14:57 -0300

    Operações Aritiméticas

---
+++ operaçõesAritiméticas.js
@@ -0,0 +1,25 @@
+console.log("Operações");
+
+const a = (2);
+const b = (5);
+const c = (3);
+
+//const equação = (a + b + c);
+//console.log(equação);
+
+
+//const equação = (a * b - c);
+//console.log(equação);
+
+//const equação = (a * b / c);
+//console.log(equação);
+
+
+const equação = (a**2 + b - c);
+console.log(equação);
+
+
+
+
+
+
