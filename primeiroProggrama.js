892d4d05dc203cc7a1488d6e278b887183d1e156
anapay <anapay42@gmail.com>
2020-05-26 16:27:51 -0300

    Adicionando comentarios para rodar um script por vez

--- primeiroProggrama.js
+++ primeiroProggrama.js
@@ -2,10 +2,9 @@ console.log ("Aprendendo js. Trabalhando com variáveis");
 
 const idade = 27;
 
-console.log (idade);
-
-const idadeEmAnos = (idade);
-console.log (idadeEmAnos + 2);
+//console.log (idade);
+//const idadeEmAnos = (idade);
+//console.log (idadeEmAnos + 2);
 //console.log (idadeEmAnos - 5); para efetuar a operação remover do coementario.//
 //console.log (idadeEmAnos * 2);//
 //console.log (idadeEmAnos / 3);//
