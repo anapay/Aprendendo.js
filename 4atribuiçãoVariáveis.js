edc176369b24e9c00898a49b86b5e4b75bda08e3
anapay <anapay42@gmail.com>
2020-05-27 15:02:29 -0300

    adicionando número para ficar por ordem

---
+++ 4atribuiçãoVariáveis.js
@@ -0,0 +1,9 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+
+const titulo = "Um amanhã de vingança, ";
+const autor = "Sidney Sheldon, ";
+const categoria = "Suspense!";
+const livroFavorito = (titulo + autor + categoria);
+
+console.log (`Meu livro preferido é ${livroFavorito}`);
