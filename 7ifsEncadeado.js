f85e0114ae0c8059afbfafe48df5a73753e764c8
anapay <anapay42@gmail.com>
2020-05-28 15:27:18 -0300

    arrumando identação(https://github.com/anapay/Aprendendo.js.git)

--- 7ifsEncadeado.js
+++ 7ifsEncadeado.js
@@ -1,35 +1,35 @@
 //ifs encadeados 
 
 
-    const filmesMaiores = new Array (
-        
-        "Árvore de Sangue (2018), Julio Medem", 
-        "Histórias Que Nosso Cinema (Não) Contava (2017), Fernanda Pessoa"
-    ) ;
+const filmesMaiores = new Array(
 
-    filmesMaiores.push(`A casa de cera `, `O massacre da serra eletrica `, `Todo mundo em Pânico` );
-   
-    const idade = 17;
-    const acompanhado = false;
-    if (idade >= 18) //se cliente for maior que >=18 execute o comando seguinte!;
-    {
-        console.log("Seja bem vindo! Divirta-se!")
-        console.log("Veja os filmes em destaque!")
-        console.log(filmesMaiores)
-    }
-    else if(acompanhado) //se não for maior faz a verificaçaõ do if se está acompanahdo
-        {
-            console.log("Menor acompanhado")
-            console.log("Seja bem vindo! Divirta-se!")
-            console.log("Veja os filmes em destaque!")
-            console.log(filmesMaiores)
+    "Árvore de Sangue (2018), Julio Medem",
+    "Histórias Que Nosso Cinema (Não) Contava (2017), Fernanda Pessoa"
+);
 
-        }
-        else //se não for maior e nem estiver acompanhado execute esse;
-        {
-            console.log("Desculpa! Menores só podem entrar acompanhados, volte acompanhado de um adulto e se divirta com a nossa sessão de filmes!")
-        };
-      
+filmesMaiores.push(`A casa de cera `, `O massacre da serra eletrica `, `Todo mundo em Pânico`);
+
+const idade = 17;
+const acompanhado = false;
+if (idade >= 18) //se cliente for maior que >=18 execute o comando seguinte!;
+{
+    console.log("Seja bem vindo! Divirta-se!")
+    console.log("Veja os filmes em destaque!")
+    console.log(filmesMaiores)
+}
+else if (acompanhado) //se não for maior faz a verificaçaõ do if se está acompanahdo
+{
+    console.log("Menor acompanhado")
+    console.log("Seja bem vindo! Divirta-se!")
+    console.log("Veja os filmes em destaque!")
+    console.log(filmesMaiores)
+
+}
+else //se não for maior e nem estiver acompanhado execute esse;
+{
+    console.log("Desculpa! Menores só podem entrar acompanhados, volte acompanhado de um adulto e se divirta com a nossa sessão de filmes!")
+};
+
 
 
 
