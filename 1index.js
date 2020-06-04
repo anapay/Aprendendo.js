7dc3269d1fa918c70fdc30a934661ce780a48019
anapay <anapay42@gmail.com>
2020-06-04 12:09:26 -0300

    Reduzindo código(https://github.com/anapay/Aprendendo.js.git)

--- 1index.js
+++ 1index.js
@@ -3,21 +3,19 @@ console.log("SisterBank")
 
 import { Cliente } from "./ACliente.js";
 import { ContaCorrente } from "./BContaCorrente.js";
-import { ContaPoupanca } from "./Contapoupanca.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./Conta.js";
 
 const cliente1 = new Cliente("Ana", 12345678910); 
 
-const cliente2 = new Cliente("Joao", 12345678911);
-
 const conta1 = new ContaCorrente(cliente1, 1001);
 conta1.depositar(102000000);
-const conta2 = new ContaCorrente(cliente2, 1001);
 
 const contaPoupanca = new ContaPoupanca(cliente1, 1001, 35000000);
 
 
 
-console.log(conta1);
-console.log(contaPoupanca);
+console.log(cliente1);
+console.log(contaPoupanca,conta1);
 
 
--- BContaCorrente.js
+++ BContaCorrente.js
@@ -1,48 +1,10 @@
-import { Cliente } from "./ACliente.js";
+import { Conta } from "./Conta.js"
 
-export class ContaCorrente {
-
+export class ContaCorrente extends Conta{
     static numeroContas = 0;
-   
-    set cliente(novoValor) {
-        if (novoValor instanceof Cliente) {
-            this._cliente = novoValor
-
-        }
-
-    }
-    get cliente() {
-        return this._cliente
-    }
-    get saldo() {
-        return this._saldo;
-    }
-
     constructor(cliente, agencia, _saldo) {
-        this.cliente = cliente;
-        this.agencia = agencia;
-        this._saldo = 0;
+        super(cliente, agencia , 0);
         ContaCorrente.numeroContas += 1;
 
-    }
-
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor
-            return valor
-        }
-
-    }
-
-    depositar(valor) {
-        if (valor <= 0) {
-            return;
-        }
-        this._saldo += valor;
-
-    }
-    trasferir(valor, conta) {
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
     }
 }
