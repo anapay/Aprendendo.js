6e9a25a06f58843b888dfc74e367589b2328d07d
anapay <anapay42@gmail.com>
2020-06-05 16:31:19 -0300

    Classe abstrata de funcionarios(https://github.com/anapay/Aprendendo.js.git

--- 1index.js
+++ 1index.js
@@ -2,10 +2,9 @@
 console.log("SisterBank")
 
 import { Cliente } from "./ACliente.js";
-import { ContaCorrente } from "./BContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
-import { Conta } from "./Conta.js";
-import { ContaSalario } from "./ContaSalario.js";
+import { ContaCorrente } from "./Contas/BContaCorrente.js";
+import { ContaPoupanca } from "./Contas/ContaPoupanca.js";
+import { ContaSalario } from "./Contas/ContaSalario.js";
 
 const cliente1 = new Cliente("Ana", 12345678910);
 const cliente2 = new Cliente("Joao", 12345678911)
@@ -17,7 +16,7 @@ const conta2 = new ContaCorrente(cliente2, 1001)
 const contaPoupanca = new ContaPoupanca(cliente1, 1001, 35000000);
 const contaPoupanca2 = new ContaPoupanca(cliente2, 1001, 350000)
 
-const conta = new Conta(cliente1, 1001, 0)
+
 
 console.log(ContaSalario)
 
