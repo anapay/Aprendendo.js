d3d1ef56e99d4f913906a4b9b4ca47e3d18dfc7d
anapay <anapay42@gmail.com>
2020-06-05 17:47:59 -0300

    Criando sistema de auenticação(https://github.com/anapay/Aprendendo.js.git

--- Funcionarios/index.js
+++ Funcionarios/index.js
@@ -3,4 +3,6 @@ import { Gerente } from "Funcionario/Gerente";
 import { Diretor } from "Funcionario/Diretor";
 
 const diretor = new Diretor ("Natalia", 10000, 12345678900);
-const gerente = new Gerente ("Tania", 5000, 123456789111)
+const gerente = new Gerente ("Tania", 5000, 123456789111);
+ 
+SistemaAutenticacao.login(diretor,"123450");
