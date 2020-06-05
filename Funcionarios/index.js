8d1054c753d2eaef4b813f6bcecba2fc8d939cfe
anapay <anapay42@gmail.com>
2020-06-05 17:16:53 -0300

    Estanciando classe funcionario(https://github.com/anapay/Aprendendo.js.git

---
+++ Funcionarios/index.js
@@ -0,0 +1,6 @@
+import { Cliente } from "./Acliente.js";
+import { Gerente } from "Funcionario/Gerente";
+import { Diretor } from "Funcionario/Diretor";
+
+const diretor = new Diretor ("Natalia", 10000, 12345678900);
+const gerente = new Gerente ("Tania", 5000, 123456789111)
